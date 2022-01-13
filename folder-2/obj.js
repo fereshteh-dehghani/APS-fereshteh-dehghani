5698bd296e04abb6f76ffb6caa93d6dd3456f7c4
fereshteh_dehghani <fdehghani666@gmail.com>
2022-01-13 11:53:43 -0800

    Update obj.js

--- folder-2/obj.js
+++ folder-2/obj.js
@@ -7,3 +7,8 @@ let user={
 }
 console.log(user.name,user.age);
 delete user.age; //for delete a property we can use 'delete' operator;
+
+user["class name"]=class2A;  //we can use multiword for key object
+
+user.isAsistent=true;        //the value can be of any data type
+
