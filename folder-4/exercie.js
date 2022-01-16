a11f9ae1003ad4c7cfd4f62cfdf3ada7e7e95c59
fereshteh_dehghani <fdehghani666@gmail.com>
2022-01-15 20:06:27 -0800

    waterBoilingPoint

--- folder-4/exercie.js
+++ folder-4/exercie.js
@@ -1,3 +1,8 @@
  score =5;
   score+5;
   console.log(score+4);
+  /////////////////////////////////////////////////////////////////////////
+  let waterBoilingPoint=100;
+  waterBoilingPoint/=2;
+  waterBoilingPoint+=10;
+  console.log(waterBoilingPoint);
