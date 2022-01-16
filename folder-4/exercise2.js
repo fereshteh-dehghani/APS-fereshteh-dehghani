ec75e4806f401425206bb48b69598e76ecf07403
fereshteh_dehghani <fdehghani666@gmail.com>
2022-01-16 02:36:38 -0800

    prompt and if condition

---
+++ folder-4/exercise2.js
@@ -0,0 +1,20 @@
+let dice1=Math.floor((Math.random()*6)+1);
+let dice2=Math.floor((Math.random()*6)+1);
+console.log(dice1 + dice2);
+let lang=prompt('which do you like language programming?');
+alert(`your choce is ${lang} thanks`);
+let ageStr=prompt("How old are you?");
+let age=Number(ageStr);
+let feedback= age>=16 ? `You're eligible to goin` :
+`you must be least 16 year old to join`; 
+alert(feedback);
+////////////////////////////////////////////////////////////////////
+////////////////////// IF CONDITION ///////////////////////////////
+/* even or odd number */
+let number=prompt('dear! please enter a number?');
+if(number%2===0){
+    alert(`${number} is a even number`);
+}else{
+    alert(`${number} is a odd number`);
+}
+
