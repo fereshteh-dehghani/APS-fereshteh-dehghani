985c9ede6c2d6d9598c4dc095b651d9d426cb7b7
fereshteh_dehghani <fdehghani666@gmail.com>
2022-01-15 20:50:49 -0800

    strings

--- folder-4/exercie.js
+++ folder-4/exercie.js
@@ -13,3 +13,20 @@
 //   this code throw error because const unchangeable
 const animal="goat candy";
 console .log(animal[5]);
+//////////////////////////////////////////////////////////////
+const city="Swansea";
+const country="walse";
+let comb=city +  country;
+console.log(comb);
+//////////////////////////////////////////////////////////////////
+let year="20000";
+console.log(year+1);//this is a string in console
+///////////////////////////////////////////////////////////////
+let strNumber="14582369";
+console.log(strNumber+14526987);//this is string in console.
+/////////////////////////////////////////////////////
+let animals="panguane";
+console.log(animals.toUpperCase());
+console.log(animals.toLowerCase());
+
+///////////////////////////////////////
