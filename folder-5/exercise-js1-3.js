dc1a458bdd81a34c7be868ebff989684fa82290d
fereshteh_dehghani <fdehghani666@gmail.com>
2022-01-17 09:35:49 -0800

    card validator

---
+++ folder-5/exercise-js1-3.js
@@ -0,0 +1,7 @@
+//////////////////////////////// Make PassWord ////////////////////////////
+let passWord='bahar 789';
+if(passWord.length>6 && !passWord.includes(" ")){
+    console.log('this password is available');
+}else{
+    console.log('this pass isnot available');
+};
