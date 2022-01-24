67cb9f8e6344034e4ff4052d9ca3332a479bbd65
fereshteh_dehghani <89343806+fereshteh-dehghani@users.noreply.github.com>
2022-01-23 20:49:44 -0800

    hi

---
+++ folder-7/spread.js
@@ -0,0 +1,9 @@
+//////////////////////// Spread ///////////////////////////////////////////////////////
+ const odd=[11,13,15,17,18];
+ const combined=[1,3,5,7,9,...odd];
+ console.log(combined);
+
+ const name=['Fereshteh','Zainab','Aida','Bahar','Omid'];
+ const studentNames=['Amin','Beny','Sara',...name];
+ console.log(studentNames);
+ 
