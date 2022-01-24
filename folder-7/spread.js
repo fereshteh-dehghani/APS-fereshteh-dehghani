0c3549c5d753bfa642e34a371d9b21c63c7bae4e
fereshteh_dehghani <89343806+fereshteh-dehghani@users.noreply.github.com>
2022-01-23 21:38:03 -0800

    finish spread

--- folder-7/spread.js
+++ folder-7/spread.js
@@ -6,4 +6,20 @@
  const name=['Fereshteh','Zainab','Aida','Bahar','Omid'];
  const studentNames=['Amin','Beny','Sara',...name];
  console.log(studentNames);
- 
+/* spread operators */
+function printName(name1,name2,name3,name4,name5){
+    console.log(name1,name2,name3,name4,name5);
+}
+printName(...name);
+/* push an array into an array*/
+const mentorsName=['Azam','Maryam','Fatemeh'];
+const allNameAps=[...mentorsName];
+
+allNameAps.push(...name);
+console.log(allNameAps);
+/* Java Script spread operator and chars */
+let night='this night is a lot of fun';
+const charsName=[...night];
+console.log(charsName);
+
+
